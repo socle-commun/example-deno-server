f53cd7c2559f9c0e63866dd15d3a9736f41b9cf9
Mistifiou <mistifiou@yahoo.fr>
2025-05-13 11:48:32 +0200

    try fix deploy

--- src/app/rest/domains/kv/routes/delete.ts
+++ src/app/rest/domains/kv/routes/delete.ts
@@ -1,7 +1,7 @@
-import { Domain } from 'https://deno.land/x/sloth@0.1.0/src/deno/apps/rest/domain.class.ts'
+import { Domain } from 'https://deno.land/x/sloth@1.0.0/src/deno/apps/rest/domain.class.ts'
 import { z } from 'npm:zod'
 import { KeyPathParamsSchema } from '../schemas.ts'
-import { kv } from 'https://deno.land/x/sloth@0.1.0/src/deno/utils/kv/instance.ts'
+import { kv } from 'https://deno.land/x/sloth@1.0.0/src/deno/utils/kv/instance.ts'
 
 export default (domain: Domain) => {
     domain
--- src/app/rest/main.ts
+++ src/app/rest/main.ts
@@ -1,5 +1,9 @@
-import $AppRest from 'https://deno.land/x/sloth@0.2.1/src/deno/apps/rest/mod.ts'
+import $AppRest from 'https://deno.land/x/sloth@1.0.0/src/deno/apps/rest/mod.ts'
 
-const { app } = await $AppRest(import.meta.url)
+const { app } = await $AppRest(import.meta.url, {
+    importCallback: (path: string) => {
+        return import(`../../../${path}`)
+    }
+})
 
 export default app;
