a94a2f46fed4e52e63b502b94a4501a0456d3f22
Mistifiou <mistifiou@yahoo.fr>
2025-05-16 09:14:40 +0200

    ✨ global import file in ext

---
+++ src/ext/imports.ts
@@ -0,0 +1,9 @@
+export { Domain } from 'https://deno.land/x/sloth@1.0.0/src/deno/apps/rest/domain.class.ts'
+export { z } from 'npm:zod'
+export { kv } from 'https://deno.land/x/sloth@1.0.0/src/deno/utils/kv/instance.ts'
+export { Context } from 'https://deno.land/x/hono@v4.3.7/context.ts'
+export { SwaggerTheme, SwaggerThemeNameEnum } from "npm:swagger-themes";
+export { SwaggerUI } from 'npm:@hono/swagger-ui';
+export { default as getEnv } from 'https://deno.land/x/sloth@1.0.0/src/deno/utils/env/mod.ts'
+export { OpenAPIHono } from 'npm:@hono/zod-openapi';
+export { cors } from 'npm:hono/cors';
